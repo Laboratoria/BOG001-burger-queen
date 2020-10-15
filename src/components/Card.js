0e98c1ad1e0d1605bcbc38daab1dd58e85a3f194
TATIANA Toro <tatianatorog@gmail.com>
2020-10-15 17:50:17 -0500

    Component button

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {H2} from './Styling';
+import Button from './Button'
 import './Card.scss';
 
 function Card() {
@@ -7,15 +8,15 @@ function Card() {
   <>
     <div className='card'>
       <H2 className='card-name'>Hamburguesa Simple</H2>
-      <div className ='card-description'>
-      <div className='card-info'>
+
       <img  className='card-info-item' src="https://firebasestorage.googleapis.com/v0/b/burger-queen-ad2b2.appspot.com/o/burger.png?alt=media&token=69768354-8921-4a33-b1f8-91e0fc0c0c46" alt="food" />
       <H2 className='card-info-price'> $10</H2>
-      </div>
-      <div className= 'card-options'>
+
+
       <input type="number" className='card-info-input' />
-      <button className='card-btn'>Agregar</button>
-      </div></div>
+      <Button cName='card-btn ' text='Agregar'> </Button>
+
+
 
     </div>
   </>
--- src/components/button.js
+++ src/components/button.js
@@ -1,21 +1,14 @@
-// Button.jsx
+
 import React from 'react';
+import './Button.scss'
 
 const Button = props => {
   return (
 	<div>
-	  <button type="button">{props.text} </button>
+	  <button  className=  {props.cName + "btn-default"} type="button">{props.text}</button>
 	</div>
   );
 };
 
 export default Button;
 
-// index.jsx
-import React from 'react';
-import Button from './components/Button';
-
-ReactDOM.render(
-  <Button text="¡Hola!" />,
-  document.getElementByid('root'),
-);
