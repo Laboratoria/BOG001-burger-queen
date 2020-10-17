a33411166d95dcbbcbbbf71c2cd492861b58fb7b
TATIANA Toro <tatianatorog@gmail.com>
2020-10-16 22:24:12 -0500

    Fixing bug useEffect

--- src/components/pages/NuevaOrden.js
+++ src/components/pages/NuevaOrden.js
@@ -1,39 +1,39 @@
+import React, { useState, useEffect } from "react";
+import Tab from "../Tab";
+import db from "../../firebaseConfig.js";
 
-import React,  { useState, useEffect }from 'react'
-import Tab from '../Tab'
-import db from '../../firebaseConfig.js'
-
-const getMenu = async (funt) => {
-  db.collection("Menu").get()
-  .then(querySnapshot => {
-
-    const tabMenu = [];
-    const modalMenu = [];
-    querySnapshot.forEach((doc) => {
-   
-      if( doc.id === 'Breakfast' || doc.id === 'Lunch'){
-       
-      tabMenu.push({ menu: Object.values(doc.data()), id: doc.id === 'Breakfast'? 'Desayuno' : 'Fuerte' })
-      } else{
-        modalMenu.push({ menu: Object.values(doc.data()), id: doc.id === 'Options'? 'Carne' : 'Adicionales' })
-      }
-    });
-  //console.log(tabMenu, modalMenu)
-  funt(tabMenu)
-  });
-};
-
-
-export default function NuevaOrden() {
-  const [menu,setMenu] = useState([]);
+const NuevaOrden = () => {
+  const [menu, setMenu] = useState([]);
 
   useEffect(() => {
-    getMenu(setMenu)
-  }, [])
+    db.collection("Menu")
+      .get()
+      .then((querySnapshot) => {
+        const tabMenu = [];
+        const modalMenu = [];
 
+        querySnapshot.forEach((doc) => {
+          if (doc.id === "Breakfast" || doc.id === "Lunch") {
+            return tabMenu.push({
+              menu: Object.values(doc.data()),
+              id: doc.id === "Breakfast" ? "Desayuno" : "Fuerte",
+            });
+          } else {
+            return modalMenu.push({
+              menu: Object.values(doc.data()),
+              id: doc.id === "Options" ? "Carne" : "Adicionales",
+            });
+          }
+        });
+        setMenu(tabMenu);
+      });
+  }, []);
   return (
     <div>
       <Tab menu={menu}></Tab>
     </div>
-  )
+  );
 };
+
+export default NuevaOrden;
+
