fd9d303724521b493f1f5918532ec51a2c176762
dcoa <dcoa@live.com>
2020-10-19 17:41:45 -0500

    modal style

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,45 +1,47 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { H1, H2 } from "../components/Styling";
 import Button from "../components/Button";
 import "./Modal.scss";
 
 export default function Modal(props) {
-  console.log(Object.keys(props.info[0]));
-  // .map((index, section) => section.id));
+    
+const title = props.info.map(section => section.id);
+const options = props.info.filter(section => section.id === title[0]).map(section => section.menu).flat();
+const toppings = props.info.filter(section => section.id === title[1]).map(section => section.menu).flat();
+console.log(props.info)
 
   return (
     <section className="overlay">
       <div className="modal">
-        <H1 className="modal-title">Escoge la carne</H1>
-        <label htmlFor="" className="modal-label">
-          <img src="" alt="Carne de pollo" className="modal-label-img" />
-          <input type="checkbox" className="modal-label-input" />
-          <H2>Pollo</H2>
+        <H1 className="modal-title">Escoge la {title[0]}</H1>
+        <div className="modal-options">
+        { options.map((item, i) => {
+            return (
+        <label htmlFor="" className="modal-label" key={i}>
+          <img src={item.img} alt="Carne de pollo" className="modal-label-img" />
+          <input type="radio" className="modal-label-input" />
+            <H2>{item.name}</H2>
         </label>
-        <label htmlFor="" className="modal-label">
-          <img src="" alt="Carne de Res" className="modal-label-img" />
-          <input type="checkbox" className="modal-label-input" />
-          <H2>Res</H2>
+            );
+        })}
+        </div>
+        
+        <H1 className="modal-title">Escoge adicionales</H1>
+        <div className="modal-options">
+        { toppings.map((item, i) => {
+            return (
+        <label htmlFor="" className="modal-label" key={i}>
+          <img src={item.img} alt="Carne de pollo" className="modal-label-img" />
+          <input type="radio" className="modal-label-input" />
+            <H2>{item.name + ' $ '+ item.price}</H2>
         </label>
-        <label htmlFor="" className="modal-label">
-          <img src="" alt="Vegetariana" className="modal-label-img" />
-          <input type="checkbox" className="modal-label-input" />
-          <H2>Vegetariana</H2>
-        </label>
-        <H1>Escoge adicionales</H1>
-        <label htmlFor="" className="modal-label">
-          {" "}
-          <img src="" alt="Queso" className="modal-label-img" />
-          <input type="checkbox" className="modal-label-input" />
-          <H2>Queso</H2>
-        </label>
-        <label htmlFor="" className="modal-label">
-          <img src="" alt="Huevo" className="modal-label-img" />
-          <input type="checkbox" className="modal-label-input" />
-          <H2>Huevo</H2>
-        </label>
-        <Button cName="btn-cancel" text="cancelar"></Button>
-        <Button cName="btn-send" text="enviar"></Button>
+            );
+        })}
+        </div>
+        <div className="modal-options" >
+        <Button cName="btn-cancel abort" text="Cancelar"></Button>
+        <Button cName="btn-send send" text="Enviar"></Button>
+        </div>
       </div>
     </section>
   );
