1922276d618e9872f1300e548a1b907a24b43180
unknown <dromerovarela776@gmail.com>
2020-11-05 07:49:36 -0500

    homvista

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import '../App.css';
+import '../css/Home.css';
+import background from '../../src/img/background.jpg'
 
 const Home = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Holi</h1>
-        </header>
+    <div className="Home">
+      <div className="Background">
+      <img src={background} alt="hello"  />
+      </div>
       </div>
    
   );
