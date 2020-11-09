abc42a962aa0eee62020abc3c884dee25f5463f6
unknown <dromerovarela776@gmail.com>
2020-11-08 20:18:43 -0500

    home

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import background from '../img/background.jpg'
-//import logo from '../img/logo.png';
+
+
+import { Link } from "react-router-dom";
+import logo from '../img/logo.png';
 
 const Home = () => {
   return (
     <div className="Home">
-      <div className="Background">
-      <img src={background} alt="hello"  />
+      <div className="Logo">
+          <img src={logo} alt="logo"/></div>
+          <div className="Soy"><h1>Soy</h1></div>
 
-      
-      </div>
-      </div>
-   
+     <div className='Buttons'>
+            <button className="NavLink">
+              
+            <Link to={'/mesero'}><h2>Mesero</h2></Link>
+            </button>
+            <button className="NavLink">
+            <Link to={'/cocinero'}><h2>Cocinero</h2></Link>
+            </button>
+           
+            </div>
+
+
+      </div>  
+
+
+
   );
 }
 
