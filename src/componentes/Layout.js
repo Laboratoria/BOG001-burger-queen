9ac74e78c51346e64ea0f4e33357ac5f710cc999
loregunner <dahara59@gmail.com>
2020-11-09 13:36:27 -0500

    principal mesero buttoms

--- src/componentes/Layout.js
+++ src/componentes/Layout.js
@@ -1,14 +1,13 @@
 import React from "react";
-import HeaderCocina from "../componentes/HeaderCocina";
 
-import '../style/Layout.css';
+
+import './styles/Layout.css';
 
 class Layout extends React.Component {
     render() {
         return (
             <div>
                 <div className= "Container"> 
-                <HeaderCocina/>
                 <div className="grid"> 
                 </div>
                 </div>
