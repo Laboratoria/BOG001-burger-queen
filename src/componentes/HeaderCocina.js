17ecd8c5bfd0ca35655ee7c2d2fa2d750f6e3d33
Natalia Rodriguez <nataliarodriguez@MacBook-Pro-de-Natalia.local>
2020-11-09 09:14:13 -0500

    Edicionn Imagen Gaseosa, pedidos

--- src/componentes/HeaderCocina.js
+++ src/componentes/HeaderCocina.js
@@ -1,8 +1,8 @@
 import React from "react";
 //import { Link } from 'react-router-dom';
-
 import logo from "../images/hamburguesa.svg";
 import "./styles/header.css";
+
 class HeaderCocina extends React.Component {
   render() {
     return (
