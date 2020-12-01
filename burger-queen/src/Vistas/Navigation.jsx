699dde5e8fb619fa0ec9f5d1b377d27d7211dab6
Gaye0523 <maya_1819@hotmail.com>
2020-12-01 14:13:11 -0500

    Hacer las vistas dinamicas

--- burger-queen/src/Vistas/Navigation.jsx
+++ burger-queen/src/Vistas/Navigation.jsx
@@ -1,34 +1,34 @@
-import React, {Fragment, useState} from 'react';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faHamburger, faTimes} from '@fortawesome/free-solid-svg-icons';
-import {Link} from "react-router-dom";
+import React, { Fragment, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHamburger, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { Link } from "react-router-dom";
+import Menu from "./Waiters.jsx";
 
-
-const Navigation =() => {
+const Navigation = () => {
     const [sidebar, setSidebar] = useState(false);
     const showSidebar = () => setSidebar(!sidebar);
 
     return (
         <Fragment>
-        <div  className="navigation">
-            <Link to='#' className='menu-bars'>
-            <FontAwesomeIcon onClick={showSidebar }  icon ={sidebar ? faTimes : faHamburger }/>
-            </Link>
-            <div className ='title'>
-            <h2>BURGER</h2>
-            <h3>QUEEN</h3>
+            <div className="navigation">
+                <Link to='#' className='menu-bars'>
+                    <FontAwesomeIcon onClick={showSidebar} icon={sidebar ? faTimes : faHamburger} />
+                </Link>
+                <div className='title'>
+                    <h2>BURGER</h2>
+                    <h3>QUEEN</h3>
+                </div>
             </div>
-        </div>
-        <div className={sidebar ? 'nav-menu-active' : 'nav-menu'}>
-            <div  className='nav-menu-items'>
-            <ul>
-                <li className='menu-bars'> 
-                </li>
-                <li className='nav-text'>Desayuno</li>
-                <li className='nav-text'>Almuerzo y Cena</li>
-            </ul>
+            <div className={sidebar ? 'nav-menu-active' : 'nav-menu'}>
+                <div className='nav-menu-items'>
+                    <ul>
+                        <Link to='#' >
+                            <li className='nav-text' onClick={<Menu showCena />} >Desayuno</li>
+                            <li className='nav-text' onClick={<Menu showCena/>} >Almuerzo y Cena</li>
+                        </Link>
+                    </ul>
+                </div>
             </div>
-        </div>
         </Fragment>
     )
 }
