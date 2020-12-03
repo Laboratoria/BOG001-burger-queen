727e6cbf5d9fe9ece7e1bf63ba5b4b143c07f48a
Natalia Rodriguez <nataliarodriguez@MacBook-Pro-de-Natalia.local>
2020-12-03 12:02:56 -0500

    firebase traer elementos

--- src/componentes/pedidosCocina.js
+++ src/componentes/pedidosCocina.js
@@ -1,16 +1,34 @@
 import React from "react";
 import "./styles/pedidosCocina.css";
+import firebase from "../firebase";
 
-class BotonesPedidos extends React.Component {
-    render() {
-        return (
-            <div className="botonPedido" type="button" >
-                <div className="numero pedido">
-                    <p>Äca va una variable Nombre"</p>
-                    <p>Hora: Aca va la hora </p>
-                </div>
-            </div>
-        )}
+const db = firebase.firestore();
+const obtenerPedidos = async () => {
+    return db.collection('pedidos')
+    .get()
+}
+
+
+function BotonesPedidos  (props) {
+
+    const querySnapshot = await obtenerPedidos();
+
+  let view = '';
+  querySnapshot.forEach ( doc => { 
+        const infoPedido = doc.data()
+        infoPedido.id = doc.id; 
+        view += <div className="botonPedido" type="button" id ={infoPedido.id}>
+        <div className="numero pedido">
+            <p>{infoPedido.nombre}</p>
+            <p>{infoPedido.orden} </p>
+        </div>
+    </div>
+    
+    })
+
+        return ( 
+            view  
+        )
 }
 
 export default BotonesPedidos;
