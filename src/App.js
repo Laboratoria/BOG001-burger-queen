e007138c4f289a3890841eb1c65550e27911d1b8
lizguro <dianyguti@gmail.com>
2020-12-07 11:01:04 -0500

    orden

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.scss';
 import Naver from './components/naver/Naver.jsx'
+import Header from './components/header/Header.jsx'
 
 function App() {
   return (
+    <Header />,
     <Naver />
   );
 }
