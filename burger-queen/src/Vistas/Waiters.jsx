ee09132610f4ce0d859a92be8b9d649bab6682ee
Angelica <angelikcu4@gmail.com>
2020-12-17 08:59:45 -0500

    cambios en firebase

--- burger-queen/src/Vistas/Waiters.jsx
+++ burger-queen/src/Vistas/Waiters.jsx
@@ -1,7 +1,9 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import Navigation from './Navigation';
 import Footer from './Footer';
 import menus from '../menu.json'
+import { db } from '../firebase'
+
 const Clients = ({ infoInput, getInput }) => {
     const updateNewTaskValue = e => getInput(e.target.value);
     return (
@@ -9,45 +11,63 @@ const Clients = ({ infoInput, getInput }) => {
             <label>Cliente: <input type="text"
                 value={infoInput}
                 onChange={updateNewTaskValue}
+
             />
             </label>
         </div>
     );
 }
-const Request = ({ showName, getOrder, setNewOrder, newBreakItem }) => {
-    
-const totalOrder = getOrder.reduce((sum, value) => (typeof value.Price == "number" ? sum + value.Price : sum), 0);
+const Request = ({ showName, getOrder, setNewOrder, newBreakItem, addRequest }) => {
+    const totalOrder = getOrder.reduce((sum, value) => (typeof value.Price == "number" ? sum + value.Price : sum), 0);
 
-const sendOrder = (e) =>{
-    e.preventDefault();
-    console.log(showName, getOrder, totalOrder,)
-}
+    const initialStateValues = {
+        name: showName,
+        order: getOrder,
+        total: totalOrder
+    }
+
+    const [values, setValues] = useState(initialStateValues)
+
+    /*const updateKitchen = (initialStateValues) => {
+        setValues(prevState => {
+            return [...prevState, initialStateValues]
+        })
+    } */
+
+    const sendOrder = () => {
+        addRequest({
+            name: showName,
+            order: getOrder,
+            total: totalOrder
+        })
+        setValues({...values })
+    }
 
     return (
         <div className="containerRequest">
             <div className="nameClients">
-            <h3>Cliente: {showName}</h3> 
+                <h3>Cliente: {showName}</h3>
             </div>
             {
                 getOrder.map(element =>
                     <h4 key={element.id}>
                         <div className="containerOrder">
-                        {element.Item}  
-                        <div className="containerPrice">
-                        {element.Us} {element.Price}
-                        </div>
+                            {element.Item}
+                            <div className="containerPrice">
+                                {element.Us} {element.Price}
+                            </div>
                         </div>
                     </h4>
                 )
             }
             <div className="totalPrice">Total = $ {totalOrder}</div>
             <div className="divEnviar"><button className="enviar" onClick={sendOrder}>Enviar</button></div>
-            </div>
-            
-        
+        </div>
+
+
     );
 }
-const ItemsBreakfast = ({ showItemBreak, showItemPrice, showItemUs, updateOrder }) => {
+const ItemsBreakfast = ({showImg, showItemBreak, showItemPrice, showItemUs, updateOrder }) => {
     return (
         <Fragment>
             {
@@ -56,7 +76,7 @@ const ItemsBreakfast = ({ showItemBreak, showItemPrice, showItemUs, updateOrder 
                     Us: showItemUs,
                     Price: showItemPrice
                 })}
-                    className="itemsBreak"> {showItemBreak}
+                    className="itemsBreak">  {showItemBreak}
                     < div className="itemsPrice" >
                         {showItemUs} {showItemPrice}
                     </div >
@@ -75,15 +95,12 @@ const MenuLunch = ({ updateOrder, showItemLunch, showLunchPrice, showLunchUs }) 
                     Us: showLunchUs,
                     Price: showLunchPrice
                 })}
-
                     className="itemsLunch"> {showItemLunch}
                     < div className="itemsPrice" >
                         {showLunchUs} {showLunchPrice}
                     </div >
                 </div >
-
             }
-
         </Fragment>
     );
 }
@@ -91,18 +108,37 @@ const MenuBreakfast = () => {
     const [cena, setCena] = useState(false);
     const showCena = () => setCena(!cena)
     const [newTaskName, setNewTaskName] = useState("")
+
     const [itemMenu, setItemMenu] = useState(menus)
     const [itemPrice, setItemPrice] = useState(menus)
+
     const [order, setOrder] = useState([])
+    const [orderComplete, setCompleteOrder] = useState([])
+
     const updateOrder = (newItemBreak) => {
         setOrder(prevState => {
             return [...prevState, newItemBreak]
         })
     }
-
-    const sendKitchen = () =>{
-        console.log("holaa")
+    const sendKitchen = async (orderMenu) => {
+        await db.collection("order").doc().set(orderMenu);
+        console.log("nueva orden")
     }
+
+    const getOrderToKitchen = () => {
+        db.collection("order").onSnapshot((querySnapshot) => {
+            const docs = [];
+            querySnapshot.forEach(doc => {
+                docs.push({...doc.data(), id:doc.id})
+            });
+            console.log(docs)
+            setCompleteOrder(docs);
+        });
+    }
+
+    useEffect(() => {
+        getOrderToKitchen();
+    }, []);
 
     return (
         <Fragment>
