20d03305e9a72895b88fdf9e7e0aa591d5495829
Angelica <angelikcu4@gmail.com>
2021-01-18 09:25:11 -0500

    añadir pedido en vista kitchen

--- burger-queen/src/Vistas/Kitchen.jsx
+++ burger-queen/src/Vistas/Kitchen.jsx
@@ -1,7 +1,51 @@
-import React, {Fragment, useState} from 'react';
+import React, {Fragment, useState, useEffect} from 'react';
 import Navigation from './Navigation';
 import Footer from './Footer';
+import { db } from '../firebase';
 
+const FirebaseKitchen = () => {
+    const [completeOrder, setCompleteOrder] = useState([])
+    const getOrderToKitchen = () => {
+        db.collection("order").onSnapshot((querySnapshot) => {
+            const docs = [];
+            querySnapshot.forEach(doc => {
+                docs.push({...doc.data(), id:doc.id})
+            });
+            setCompleteOrder(docs);
+        });
+    }
+    
+    useEffect(() => {
+        getOrderToKitchen()   
+    }, []);
+
+    return ( 
+        <Fragment>
+            <div className="containerOrder">
+            {
+            completeOrder.map(item => {
+                return (
+                    <div className="backgroundOrder">
+                        <div className="containerName">
+                        Cliente: {item.name}
+                        </div>
+                        {item.order.map(e => 
+                            {
+                                return(
+                                    <div className="orderKitchen">
+                                        {e.Item}
+                                    </div>
+                                )
+                            })}
+                    </div>
+                    
+                )
+            })
+            }
+        </div>
+        </Fragment>
+    );
+}
 
 
 const BackgroundKitchen = () => {
@@ -9,6 +53,7 @@ const BackgroundKitchen = () => {
     <Fragment>
 <div className="backgroundKitchen">
         <Navigation/>
+        <FirebaseKitchen/>
         <Footer/>
     </div>
     </Fragment>
