291a2f9b0d129f265a85c8f309dbac2e664b6059
Lina Ureña <lina8maria8@hotmail.com>
2021-01-23 20:25:53 -0500

    Order description of card selected

--- my-app/src/components/organisms/ToPrepareList.jsx
+++ my-app/src/components/organisms/ToPrepareList.jsx
@@ -23,9 +23,25 @@ const ToPrepareList = (props) =>{
         });
         setOrders(docs)
       });
-
   }
 
+  const [orderDescription, setOrderDescription] = useState (['']);
+
+  const loadOrderDescription = async (id) => {
+    let idOrder = id;
+    console.log(idOrder)
+    db.collection("br-orders").onSnapshot(
+      (querySnapshot) => {
+        const docsData = [];
+        querySnapshot.forEach(doc => {
+          if(doc.id === idOrder){
+            docsData.push({...doc.data(), id:doc.id});
+          }
+        });
+        console.log(docsData)
+        setOrderDescription(docsData);
+      });
+  }
 
   useEffect(() => {
     loadOrders();
@@ -34,13 +50,20 @@ const ToPrepareList = (props) =>{
   return(
     <div className={`${Style.statusContent}`}>
         {orders.map((order) => (
-          <div className={`${Style.orderCard}`} key={order.id}>
+          <div className={`${Style.orderCard}`} key={order.id} onClick={() => loadOrderDescription (order.id)} >
             <div className={`${Style.tableBox}`}>
               <h3 className={`${Style.dataTable}`}>Table {order.table}</h3>
             </div>
             <div className={`${Style.stateBoxDanger}`}>
               <h4 className={`${Style.dataState}`}>{order.state}</h4>
             </div>
+          </div>
+        ))}
+        {orderDescription.map((order) => (
+          <div key={order.id}>
+            <h3>Table # {order.table}</h3>
+            <p>{order.state}</p>
+            <button>Start</button>
           </div>
         ))}
     </div>
